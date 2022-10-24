4ffba5fea9afd5fde3b17bd1e6b36793c1fb946a
ysh <1043701463@qq.com>
2022-10-24 16:49:12 +0800

    小修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -68,7 +68,7 @@ export default {
     // }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('密码应不少于6位数'))
       } else {
         callback()
       }
